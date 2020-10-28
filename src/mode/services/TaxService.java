9efe016935870a099859a43fd5f63f9ba22ab811
Caique Moreira <caique_moreira7@hotmail.com>
2020-10-28 13:17:12 -0300

    solução com interface

--- src/mode/services/TaxService.java
+++ src/mode/services/TaxService.java
@@ -9,13 +9,6 @@ package mode.services;
  *
  * @author caiqu
  */
-public class BrazilTaxService implements TaxService{
-    
-    public double tax (double amount){
-       if(amount <= 100.0){
-           return amount * 0.2;
-       } else{
-           return amount * 0.15;
-       }
-    }
+public interface TaxService {
+    double tax (double amount);
 }
